526847ce5e3a01b888121b6bfa585910449f6bcc
Omar Ahmed <me@mrg.sh>
2023-03-26 06:35:31 +0200

    feat: look for .monoenv.yaml file by default

--- src/bin/run.ts
+++ src/bin/run.ts
@@ -6,9 +6,4 @@ import { loadEnvFromConfigFile } from "../index";
 const argv = minimist(process.argv.slice(2));
 const configPath = argv.config || argv.c;
 
-if (!configPath) {
-  console.error("missing [ --config | -c ] argument");
-  process.exit(1);
-}
-
 loadEnvFromConfigFile(configPath);
--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,20 @@ export function loadEnv() {
   processProjectFile(projectFile.path);
 }
 
-export function loadEnvFromConfigFile(projectPath: string) {
-  if (!fileExists(projectPath)) {
-    console.error(`[ ${projectPath} ] does not exist`);
+export function loadEnvFromConfigFile(projectPath: string | undefined) {
+  const path = projectPath || getProjectFile()?.path;
+
+  if (!path) {
+    console.error(
+      "either have a '.monoenv.yaml' file on your root directory or supply your monoenv config file with [ --config | -c ] argument"
+    );
     process.exit(1);
   }
 
-  processProjectFile(projectPath);
+  if (!fileExists(path)) {
+    console.error(`[ ${path} ] does not exist`);
+    process.exit(1);
+  }
+
+  processProjectFile(path);
 }
